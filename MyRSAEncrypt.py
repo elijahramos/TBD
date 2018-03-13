d8f56e4ef6aa41e1d2723e7be0c14571e18b35c2
Kevin Simon <rocpadge@gmail.com>
2018-03-13 01:37:00 -0700

    a syntax error, there is unfinished code.

--- MyRSAEncrypt.py
+++ MyRSAEncrypt.py
@@ -1,16 +1,21 @@
+# Cryptology module imports
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import serialization
+
 import MyfileEncrypt
 
 def norm(filePath,publicKeyPath):
 	g=myfileEncrypt(filePath)
 	
-	#TODO: encrypt the key with RSA and OAEP padding.
-	# the result goes in slot #0
-	
-	
-	
-	return [0,g[0],g[1],g[3]]
-def inv(RSAC,C,IV,filePath):
-	
-	#TODO: decryption loal
-	
-	return 0 # the deciphered data
+	k = #public key obj, Im looking for a way to turn plain text public keyis in something this uses.
+	c = k.encrypt(m,padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None))
+	#write to file.
+	return [c,g[0],g[1],g[3]]
+def inv(RSAC,C,IV,ext,kPath):
+	k = serialization.load_pem_private_key(open(kPath, "rb").read(),password=None,backend=default_backend())
+	d = k.decrypt(RSAC,padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None))
+	# also write to file
+	return d # the deciphered data
