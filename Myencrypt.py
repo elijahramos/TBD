b4f65ffbfba0b62d71a8e81dc84d14f09483006e
Kevin Simon <rocpadge@gmail.com>
2018-03-12 23:16:53 -0700

    Okay, quickfix just happend.

--- Myencrypt.py
+++ Myencrypt.py
@@ -5,14 +5,9 @@ import os # some of this needs the OS's PRNG
 import cnsts
 
 def norm(y, key): # use CBC (AES), also the Key has to be less than 32bytes long (256bit), also the message is a byte string.
-	if(len(y)%cnsts.IVLengthExpt!=0):
-		raise Exception("byte count has gotta be a multiple of 16")
-		# I may wana go back and add some kind of padding thingy,
-	
 	padder = padding.PKCS7(cnsts.padding).padder()
 	padded_data = padder.update(y)
 	padded_data += padder.finalize()
-	
 	if(len(key)<cnsts.keyLengthExpt): raise Exception("key's gotta be AT LEAST 32 bytes")
 	backend = default_backend()
 	IV = os.urandom(cnsts.IVLength)
@@ -26,8 +21,6 @@ def inv(C,IV,key):
 	cipher = Cipher(algorithms.AES(key), modes.CBC(IV), backend=backend)
 	decryptor = cipher.decryptor()
 	dun = decryptor.update(C) + decryptor.finalize()
-	
 	unpadder = padding.PKCS7(cnsts.padding).unpadder()
 	data = unpadder.update(dun)
-	
 	return (data + unpadder.finalize())
--- cnsts.py
+++ cnsts.py
@@ -1,5 +1,5 @@
 keyLength=32
 IVLength=16
 keyLengthExpt=32
-IVLengthExpt=16
+IVLengthExpt=16 # unused
 padding=128 #TODO: solve the padding issue
