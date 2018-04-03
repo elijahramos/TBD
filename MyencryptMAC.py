b03307481942b456250e5de8f51668c8e8968e41
SNERFOIL <rocpadge@gmail.com>
2018-04-02 20:03:20 -0700

    I added new modules that are like the old ones but they handle HMAC or somethin', also added new options to the test.py to showcase what it does.

---
+++ MyencryptMAC.py
@@ -0,0 +1,52 @@
+# these are the cryptography suites, they handle most of the magics
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+#HMAC, for your health
+from cryptography.hazmat.primitives import hashes, hmac
+
+import os # some of this needs the OS's PRNG
+import cnsts # Constants! for your health.
+
+# uses CBC (AES), also the Key has to be less than 32bytes long (256bit), also the message is a byte string. not to be confused with regular strings.
+def norm(y, key, mKey):#forgive my unclear variables, but y is the Plain text variable. this was ported from a monolithic version of the program.
+	
+	#this is the padder, it makes the plain txt kosher for the encryption algo.
+	padder = padding.PKCS7(cnsts.padding).padder()# makes the padding obj
+	padded_data = padder.update(y)# an update of sorts
+	padded_data += padder.finalize()# puts some padding data on the plain txt
+	
+	# exception is self explanitory
+	if(len(key)<cnsts.keyLengthExpt): raise Exception("key's gotta be AT LEAST 32 bytes")
+	
+	backend = default_backend()# this is required a few lines down
+	
+	IV = os.urandom(cnsts.IVLength)# gather only the FINEST RNG for our purposes.
+	cipher = Cipher(algorithms.AES(key), modes.CBC(IV), backend=backend)# makes the encryption algo.
+	encryptor = cipher.encryptor()# and then the encryptor
+	C = encryptor.update(padded_data) + encryptor.finalize() # then we bake the cipher txt.
+	
+	#we encrypted, now we MAC. er, setup the mac thingy.
+	mac = hmac.HMAC(mKey, hashes.SHA256(), backend=default_backend())
+	mac.update(C) # put the cypher txt in the HMAC
+	
+	return [C,IV,mac.finalize()]# puts the cipher txt, IV value, and the tag in a 3 slot array list, this is for manipulation outside the function.
+
+def inv(C,IV,key,tag,mKey):# basically, the decryption thingy.
+	
+	# a verification step
+	mac = hmac.HMAC(mKey, hashes.SHA256(), backend=default_backend())
+	mac.update(C) # put the cypher txt in the HMAC
+	try: mac.verify(tag) # try to verify this.
+	except: return None # verification failed so we dont do shit.
+	
+	backend = default_backend()# again with the backend
+	cipher = Cipher(algorithms.AES(key), modes.CBC(IV), backend=backend)# we have to REBUILD the same algo.
+	decryptor = cipher.decryptor()# you know the drill.
+	dun = decryptor.update(C) + decryptor.finalize()# decrypt that stuff
+	
+	#but wait, we also have to remove the padding.
+	unpadder = padding.PKCS7(cnsts.padding).unpadder()# make the unpadder, Uh-gain.
+	data = unpadder.update(dun)# removin...
+	
+	return (data + unpadder.finalize())# now we have our text, in all of its plain glory.
--- test.py
+++ test.py
@@ -13,6 +13,8 @@ import cnsts
 import MyRSAEncrypt
 import MyfileEncrypt
 import Myencrypt
+import MyfileEncryptMAC
+import MyencryptMAC
 import pickle   #use pickle as opposed to JSON due to issues with byte strings
                 #pickle.dumps(theThingYouWantToBecomeAByteString) returns a byte string.
                 #pickle.loads(byteString) will do the inverse of that.
@@ -20,7 +22,7 @@ import pickle   #use pickle as opposed to JSON due to issues with byte strings
 def main():
 	runTiem = True
 	while(runTiem):
-		print("0: exit\n1: encrypt(RSA)\n2: decrypt(RSA)\n3: makekeys\n4: basicEncrypt\n5: basicDecrypt\n--> ",end='')
+		print("0: exit\n1: encrypt(RSA)\n2: decrypt(RSA)\n3: makekeys\n4: basicEncrypt\n5: basicDecrypt\n6: encryptMAC(RSA)\n7: decryptMAC(RSA)\n--> ",end='')
 		u = input()
 		if(u=='0'): runTiem=False
 		elif(u=='1'): option1()
@@ -28,6 +30,8 @@ def main():
 		elif(u=='3'): option3()
 		elif(u=='4'): option4()
 		elif(u=='5'): option5()
+		elif(u=='6'): option6()
+		elif(u=='7'): option7()
 
 def option1():
 	print("Select file to encrypt: ",end='')
@@ -69,7 +73,7 @@ def option2():
 def option3(): #key generator
 	print("where do you wish to save the keys?(dont type ext, just filename): ",end='')
 	u = input()
-	kg = rsa.generate_private_key(public_exponent=65537,key_size=2048,backend=default_backend())
+	kg = rsa.generate_private_key(public_exponent=65537,key_size=3072,backend=default_backend())#TODO: increase key size.(was 2048, next big sizes are 3072 and 15360)
 	#pk = private_key.private_bytes(encoding=serialization.Encoding.PEM,format=serialization.PrivateFormat.PKCS8,encryption_algorithm=serialization.BestAvailableEncryption(b'mypassword'))
 	prvkNoENC = kg.private_bytes(encoding=serialization.Encoding.PEM,format=serialization.PrivateFormat.PKCS8,encryption_algorithm=serialization.NoEncryption())
 	pubkNoENC = kg.public_key().public_bytes(encoding=serialization.Encoding.PEM,format=serialization.PublicFormat.SubjectPublicKeyInfo)
@@ -104,6 +108,67 @@ def option5():
 	open((cutt[0]+"."+g[1]),"wb").write(g[0])# dere it is
 	os.remove(filePath) # delete the file.
 	os.remove(keyPath) # delete the file.
+	print("done",end='')
+	input()
+
+def option6():
+	print("Select file to encrypt: ",end='')
+	filePath = input()
+	print("Select public key: ",end='')
+	keyPath = input()
+	
+	f = MyfileEncryptMAC.norm(filePath) #filepath = [C, IV, key,fileDir,fileName,fileExt,tag,HMAC]
+	
+	
+	ccat = pickle.dumps([f[2],f[7]]) # concatinate the key and HMAC together, my special way.
+	
+	g = b''
+	while(ccat!=b''):# so this whole bit was to get around a 241byte limitation of RSA encryption.
+		#print(str(len(ccat[:128])),end=', ')
+		thBit = MyRSAEncrypt.norm(ccat[:128],keyPath) #encrypted key
+		ccat = ccat[128:]
+		g = g + thBit # the encrypted bit is double in size '256'
+		#print(str(len(thBit)),end='')
+		#input()
+	# the trick here was to only RSAencrypt the conactination of the key and HMAC key(<- the phat fucc rite here), 
+	
+	open((f[3]+f[4]+".ukn"),"wb").write(pickle.dumps([g,f[0],f[1],f[5],f[6]])) #[RSAC,C,IV,fileext,tag]
+	os.remove(filePath) #delete the file.
+	
+	print("done",end='')
+	input()
+	
+def option7():
+	print("Select file to decrypt: ",end='')
+	filePath = input()
+	print("Select private key: ",end='')
+	keyPath = input()
+	
+	fileObj = open(filePath, "rb") #open file, and save the referance for later
+	location = filePath.rstrip(os.path.basename(fileObj.name)) #cut off the file's directory
+	cutFile = (os.path.basename(fileObj.name)).partition('.') #place the file's name into a list [fileName, ., ext]
+	fileString = fileObj.read()
+	
+	g = pickle.loads(fileString) #gets the above [RSAC, C, IV, ext,tag]
+	gg = g[0] # this value needs to get un pickled [key,HMAC] <- but this is encrypted in a funny way.
+
+	bb = b''
+	while(gg!=b''):
+		#print(str(len(gg[:256])),end=', ')
+		thBit = MyRSAEncrypt.inv(gg[:256],keyPath) #encrypted key
+		gg = gg[256:]
+		bb = bb + thBit # the encrypted bit is double in size '256'
+		#print(str(len(thBit)),end='')
+		#input()
+	
+	thisDummy = pickle.loads(bb)
+	
+	buff = MyencryptMAC.inv(g[1],g[2],thisDummy[0],g[4],thisDummy[1])
+	#we would use MyfileEncrypt if JUST the cipher text was stored in a file
+	#but it isnt, its pickled with the IV, RSAC, and ext for convienence.
+	open((cutFile[0]+'.'+g[3]),"wb").write(buff)
+	os.remove(filePath) # delete the file.
+	
 	print("done",end='')
 	input()
 
