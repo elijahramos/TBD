f872a0de135e98aca7d9b02679c97da88045430d
Snerfoil <rocpadge@gmail.com>
2018-04-11 20:51:28 -0700

    more edits, I dono what to say here anyway.

--- cnsts.py
+++ cnsts.py
@@ -1,3 +1,4 @@
+import os
 keyLength=32
 IVLength=16
 keyLengthExpt=32
@@ -5,4 +6,5 @@ IVLengthExpt=16 # unused
 padding=128
 kPrv = "k_prv.pem"
 kPub = "k_pub.pem"
+
 #Python3 has no "constants" but this does the job.
