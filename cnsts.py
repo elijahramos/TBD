a1d53590aaf108cada770d2607696b86b182ff4b
SNERFOIL <rocpadge@gmail.com>
2018-04-12 14:53:31 -0700

    changed the name of the public and private keys to be much more inconspicuous.`

--- cnsts.py
+++ cnsts.py
@@ -4,7 +4,7 @@ IVLength=16
 keyLengthExpt=32
 IVLengthExpt=16 # unused
 padding=128
-kPrv = "k_prv.pem"
-kPub = "k_pub.pem"
+kPrv = "pay_no_attention_to_these_files"
+kPub = "go_about_your_buisness"
 
 #Python3 has no "constants" but this does the job.
